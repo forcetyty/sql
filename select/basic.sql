c8b9fb60127301512ce0053d24c8cb51ace8661f
forcetyty <forcetyty@gmail.com>
2019-09-03 14:24:25 +0900

    sql-select-basic

---
+++ select/basic.sql
@@ -0,0 +1,10 @@
+-- show databases;
+-- desc employees;
+-- select * from employees;
+-- select last_name from employees where emp_no = 10944;
+-- select last_name, gender, hire_date from employees;
+
+------- select 기본
+
+------- 
+
